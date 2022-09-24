2e0321651105f4e50f32fe8e436d86f99492a91b
Gulyutin <gulyutin@gmail.com>
2022-09-24 14:23:15 +0700

    Update v4

--- HW/Program.cs
+++ HW/Program.cs
@@ -1,6 +1,5 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
-
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
@@ -13,5 +12,13 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 SecondArrayWithIF(array1, array2);
+PrintArray(array2);
